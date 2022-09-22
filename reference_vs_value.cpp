80b9a4642206ca73f24e437acbaefddc8b700c89
Abhishek Sharma <113858734+sha-abhishek@users.noreply.github.com>
2022-09-22 02:09:54 -0400

    Add files via upload

---
+++ reference_vs_value.cpp
@@ -0,0 +1,89 @@
+/*  Nick Sweeting 2013/10/09
+    References vs Values in C++
+    MIT License
+    Takes input and removes puctuation and spaces, using two different methods.
+    It is referred from: https://github.com/pirate/Cpp-Data-Structures/
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+// returns true if input character q is puctuation, else false
+bool ispunctuation(char q) {
+    // complete the functions here ...
+    if (q!=32){
+        for(int i = 48; i<57; i++){
+            if (q == i) {return  false;}
+        }
+        for(int i = 65; i<91; i++){
+            if (q == i){return false;}
+        }
+        for(int i = 97; i<123; i++){
+            if (q == i) {return  false;}
+        }
+
+        return true;
+    }
+    else{return false;}
+
+}
+
+char* modifyAndCopy(char* raw_input, char* empty_input) {
+    // complete the functions here ...
+    int a = strlen(raw_input);
+    
+    int j = 0;
+    for (int i = 0; i < a; i++){
+        if(ispunctuation(raw_input[i])){
+        }
+        else {
+            empty_input[j] = raw_input[i];
+            j++;
+        }
+    }
+    return empty_input;
+}
+
+
+char* modifyInPlace(char *raw_input) {
+    // complete the functions here ...
+    int a = strlen(raw_input);
+    char* mod_input = raw_input; //mod_array;
+    
+    int j = 0;
+    for (int i = 0; i < a; i++){
+        if(ispunctuation(raw_input[i])){
+        }
+        else {
+            mod_input[j] = raw_input[i];
+            j++;
+        }
+        //cout << "value of i: " << i << endl;
+        //cout << "value of j: " << j << endl;
+    }
+    for (int k=j; k<a; k++){
+        mod_input[k] = {0};
+    }
+    return raw_input;
+}
+
+int main() {
+    // user input
+    char raw_input[80] = {0};
+    cout << "Please input something with punctuation in it: ";
+    cin.getline(raw_input,80);
+    cout << "length of raw input: " << strlen(raw_input) << endl;
+
+    char mod_array[strlen(raw_input)] = {0};
+    cout << "Modify and Copy: " << endl;
+    cout << "Original: " << raw_input << endl;
+    cout << "Modified: " << modifyAndCopy(raw_input, mod_array)<< endl << endl;
+
+    cout << "Modify in Place: " << endl;
+    cout << "Original: " << raw_input << endl;
+    cout << "Modified: " << modifyInPlace(raw_input) << endl;
+    
+    return 0;
+}
---
+++ vectors.cpp
@@ -0,0 +1,122 @@
+/*  Nick Sweeting 2013/10/08
+    Student List (OOP)
+    MIT Lisence
+    g++ Vectors.cpp -o main && ./main
+    Example of using vectors to store a list of students and their GPAs.
+    It is referred from: https://github.com/pirate/Cpp-Data-Structures/
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+struct Student {
+    string firstName;
+    string lastName;
+    int studentID;
+    double GPA;
+};
+
+// function to convert float to string with defined precision
+string fltostr(double num){
+    ostringstream strobj;
+    strobj << fixed;
+    strobj << setprecision(2);
+    strobj << num ;
+    string s = strobj.str();
+    return s;
+
+};
+
+
+void printStudents(vector<Student> students) {
+
+    int width = 14;
+    string head_fn = "First Name";
+    string head_ln = "Last Name";
+    string head_id = "Student ID";
+    string head_gpa = "Student GPA";
+    int h_w1 = width - head_fn.length();
+    int h_w2 = width - head_ln.length();
+    int h_w3 = width - head_id.length(); 
+    int h_w4 = width - head_gpa.length();
+
+    cout << endl; 
+    cout << " | " << head_fn <<setw(h_w1)<< " | " << head_ln <<setw(h_w2)<< " | " << head_id <<setw(h_w3)<< " | " << head_gpa <<setw(h_w4)<<" | " << endl; 
+
+    for (int i=0; i<students.size(); i++)
+    {
+        string fnstr = students[i].firstName;
+        string lnstr = students[i].lastName;
+        string idstr =  to_string(students[i].studentID);
+        string gpastr = fltostr(students[i].GPA);
+        int w1 = width - fnstr.length();
+        int w2 = width - lnstr.length();
+        int w3 = width - idstr.length();
+        int w4 = width - gpastr.length();
+
+        cout << " | " << fnstr << setw(w1)<< " | " << lnstr << setw(w2)<< " | " << idstr << setw(w3) <<" | " << gpastr <<setw(w4) << " | " << endl;
+    }
+};
+
+vector<Student> addStudent(vector<Student> students) {
+
+    Student newStudent;
+
+    cout << "First Name: ";
+    cin >> newStudent.firstName;
+    cout << "Last Name: ";
+    cin >> newStudent.lastName;
+    cout << "ID: ";
+    cin >> newStudent.studentID;
+    cout << "GPA: ";
+    cin >> newStudent.GPA;
+
+    students.push_back({newStudent.firstName, newStudent.lastName, newStudent.studentID, newStudent.GPA});
+
+    return students;
+}
+
+vector<Student> delStudent(vector<Student> students) {
+    int studentIDtoDel;
+    cout << "ID of student to delete: ";
+    cin >> studentIDtoDel;
+
+    cout << "ID to delete: " << studentIDtoDel << endl;
+
+
+    for(int i = 0; i<students.size(); i++){
+        if(students[i].studentID == studentIDtoDel){
+            students.erase(students.begin()+i);
+        }
+    }
+
+    return students;
+}
+
+int main() {
+    vector<Student> students;
+    string input;
+
+    while (true) {
+        cout<<"Input operation: ";
+        cin >> input;
+
+        if (input == "ADD" || input == "a" || input == "add") {
+            students = addStudent(students);
+        }
+        else if (input == "PRINT" || input == "p" || input == "print") {
+            printStudents(students);
+        }
+        else if (input == "DELETE" || input == "d" || input == "delete") {
+            students = delStudent(students);
+        }
+        else if (input == "QUIT" || input == "q" || input == "quit") {
+            return 0;
+        }
+    }
+}
+
