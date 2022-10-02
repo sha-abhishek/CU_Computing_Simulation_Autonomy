d94bbf110e3e47ab2cb571945801a871a6824b18
ABHISHEK SHARMA <asharm5@clemson.edu>
2022-10-02 01:40:54 -0400

    Q2 completed with DFS using recursion, Q1 still needs optimisation

--- HW3/Question1.py
+++ HW3/Question1.py
@@ -1,1 +1,19 @@
-print("question")
+def solution(list, num): 
+    a=0 
+    b=0 
+    '''type in your solution, find a and b in array that a+b=num'''
+    for i in range(len(list)-1):
+        for j in range(i+1,len(list)):
+            a = list[i]
+            b = list[j]
+            if list[i] + list[j] == num:
+                return a, b
+    print ("sum for", num, "not found")
+     
+  
+#numbers = [0, 21, 78, 19, 90, 13]
+
+numbers = [0,21,78,19,90,13,23,22,3,1,5,8,2] 
+print(solution(numbers, 21)) 
+print(solution(numbers, 25)) 
+print(solution(numbers,11))
---
+++ HW3/Question2.py
@@ -0,0 +1,36 @@
+class TreeNode(object):
+    """ Definition of a binary tree node."""
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+def solution(root):
+    '''type in your solution'''
+    depth = 0
+    if root == None:
+        return depth
+
+    else:
+        depth_left = solution(root.left)
+        depth_right = solution(root.right)
+
+        if depth_right > depth_left:
+            depth = depth_right + 1
+            
+        else:
+            depth = depth_left + 1
+        
+    return depth
+
+a15=TreeNode(15)
+a7=TreeNode(7)
+a20=TreeNode(20)
+a9=TreeNode(9)
+a3=TreeNode(3)
+a5=TreeNode(5)
+a15.left=a5
+a20.left=a15
+a20.right=a7
+a3.left=a9
+a3.right=a20
+print(solution(a3))
