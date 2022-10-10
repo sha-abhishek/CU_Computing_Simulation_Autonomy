d02f96f499d1d5f5fa810fc7a25435e9e36421af
ABHISHEK SHARMA <asharm5@clemson.edu>
2022-10-10 15:32:17 -0400

    Q1 modified to use hash table

--- HW3/Question1.py
+++ HW3/Question1.py
@@ -1,19 +1,18 @@
-def solution(list, num): 
-    a=0 
-    b=0 
-    '''find a and b in array that a+b=num'''
-    for i in range(len(list)-1):
-        for j in range(i+1,len(list)):
-            a = list[i]
-            b = list[j]
-            if list[i] + list[j] == num:
-                return a, b
-    print ("sum for", num, "not found")
-     
-  
-#numbers = [0, 21, 78, 19, 90, 13]
-
-numbers = [0,21,78,19,90,13,23,22,3,1,5,8,2] 
-print(solution(numbers, 21)) 
-print(solution(numbers, 25)) 
-print(solution(numbers,11))
+def solution(list, num):
+    hasht = {}
+    
+    for a in list:
+        b = num - a
+        #print(hasht)
+        if b>=0:
+            if b in hasht:
+            #print(hasht[b],b)
+                return print(hasht[b],b)
+            
+            hasht[a] = b
+            #print(hasht)
+    return print("sum for", num, "not found")
+    
+    
+list = [1,5,7,0,11,15,22,25]
+solution(list,26)
